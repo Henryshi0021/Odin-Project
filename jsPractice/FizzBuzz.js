15616fb42444a7355098927b1e0dfa0290fdea3d
Henry Shi <h.shiere@gmail.com>
2023-05-28 21:10:02 -0400

    Updated to run on terminal

--- jsPractice/FizzBuzz.js
+++ jsPractice/FizzBuzz.js
@@ -1,13 +1,19 @@
-let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
-
-for (let i = 1; i <= answer; i++) {
-  if((i%3 === 0) && (i%5 === 0)){
-    console.log("FizzBuzz");
-  }else if(i%5 === 0){
-    console.log("Buzz");
-  }else if(i%3 === 0){
-    console.log("Fizz")
-  }else{
-    console.log(i);
-  }
-}
+const readline = require('readline');
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+r1.question("Please enter the number you would like to FizzBuzz up to: ",(answer)=>{
+    for (let i = 1; i <= answer; i++) {
+        if((i%3 === 0) && (i%5 === 0)){
+          console.log("FizzBuzz");
+        }else if(i%5 === 0){
+          console.log("Buzz");
+        }else if(i%3 === 0){
+          console.log("Fizz")
+        }else{
+          console.log(i);
+        }
+      }
+      r1.close();
+});
